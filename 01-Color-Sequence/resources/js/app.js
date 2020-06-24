1ed4ab047d217f4449abbd88de3d2cdfb00f2010
Fabian Zeiher <fzeiher@gmail.com>
2020-06-24 10:26:12 +0200

    add solution for color sequence

--- 01-Color-Sequence/resources/js/app.js
+++ 01-Color-Sequence/resources/js/app.js
@@ -1,9 +1,29 @@
 /* eslint-env browser */
 
-const NUMBER_OF_BOXES = 20;
+const NUMBER_OF_BOXES = 20,
+  NUMBER_OF_CLICKS = 10;
+
+var boxArray = [];
 
 function init() {
   initBoard();
+  setBoxClickListener();
+}
+
+function setBoxClickListener() {
+  let boardEl = document.querySelector("#board");
+  boardEl.addEventListener("click", (event) => {
+    if (event.target.classList.contains("box")) {
+      handleBoxClick(event.target);
+    }
+  });
+}
+
+function handleBoxClick(boxEl) {
+  boxArray.push(boxEl);
+  if (boxArray.length === NUMBER_OF_CLICKS) {
+    replaySequence();
+  }
 }
 
 function initBoard() {
@@ -24,6 +44,24 @@ function getRandomColor() {
   colors.push(((Math.random() * 255) + 255) / 2);
   colorString = colorString.replace("{{COLOR}}", colors.join(","));
   return colorString;
+}
+
+function replaySequence() {
+  let boxNum = 11 - boxArray.length;
+  console.log("highlighting box " + boxNum);
+
+  boxArray[0].classList.add("active");
+  setTimeout(function() {
+    boxArray[0].classList.remove("active");
+    boxArray.shift();
+
+    if (boxArray.length === 0) {
+      console.log("sequence_finished");
+      return;
+    }
+
+    replaySequence();
+  }, 1000);
 }
 
 init();
